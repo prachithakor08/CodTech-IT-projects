d36082d0adeb8189de4e6650baa1c53a41857e33
prachithakor08 <157780262+prachithakor08@users.noreply.github.com>
2024-01-29 16:26:45 +0530

    Weather application
    This file contains all the related images and files for developing the weather app.

--- script.js
+++ script.js
@@ -1,38 +1,39 @@
-const Inputbox=document.getElementById("input-box");
-const listcontainer=document.getElementById("list-container");
-
-function addtask()
-{
-    if(Inputbox.value === '')
-    {
-        alert("you must write something!");
-    }else{
-        let li=document.createElement("li");
-        li.innerHTML=Inputbox.value;
-        listcontainer.appendChild(li);
-        let span=document.createElement("span");
-        span.innerHTML="\u00d7";
-        li.appendChild(span);
+const options = {
+    method:'GET',
+    headers: {
+        'X-RapidAPI-Key': '71f2343f4fmsh11715811ba9038dp127c57jsn0695419c601c',
+        'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
     }
-    Inputbox.value="";
-    savedata();
-}
-listcontainer.addEventListener("click",function(e){
-        if(e.target.tagName === "LI")
-        {
-            e.target.classList.toggle("checked");
-        }
-        else if(e.target.tagName === "SPAN")
-        {
-            e.target.parentElement.remove();
-        }
-},false);
-function savedata()
-{
-    localStorage.setItem("data",listcontainer.innerHTML)
-}
-function displaylist()
-{
-    listcontainer.innerHTML =localStorage.getItem("data");
-}
-displaylist();
+    };
+    const getWeather =(city)=>{
+        cityName.innerHTML = city
+    fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city='+city,options)
+    .then(response=>response.json())
+    .then(response=> {
+        
+        
+        console.log(response)
+        // cloud_pct.innerHTML= response.cloud_pct
+    temp.innerHTML= response.temp
+    temp2.innerHTML= response.temp
+    feels_like.innerHTML= response.feels_like
+    humidity.innerHTML= response.humidity
+    humidity2.innerHTML= response.humidity
+    min_temp.innerHTML= response.min_temp
+    max_temp.innerHTML= response.max_temp
+    wind_speed.innerHTML= response.wind_speed
+    wind_degrees.innerHTML= response.wind_degrees
+    wind_degrees2.innerHTML= response.wind_degrees
+    sunrise.innerHTML= response.sunrise
+    sunset.innerHTML= response.sunset
+    })
+        
+    .catch(err =>console.log(err));
+    
+    }
+    
+    submit.addEventListener("click",(e)=>{
+        e.preventDefault()
+        getWeather(city.value)
+    })
+    getWeather("Delhi")
